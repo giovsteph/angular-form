0e29438921ccb5e1e6fb1ba26f440164234219b3
giovsteph <giov.ponce@gmail.com>
2020-05-09 18:24:15 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  log(x) {console.log(x);}
+  // firstName = '';
+  // lastName = '';
+  // email = '';
+  // password = '';
+  // confirmPassword = '';
+  // country = '';
+  // company = '';
+  // companyWebsite = '';
+}
